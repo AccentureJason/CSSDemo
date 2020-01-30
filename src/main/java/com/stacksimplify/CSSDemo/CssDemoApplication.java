f501d2fdba368763e87f51771bb7cc557dc5e709
Jones <jason.b.jones@accenture.com>
2020-01-30 15:53:43 -0500

    Initial

---
+++ src/main/java/com/stacksimplify/CSSDemo/CssDemoApplication.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.CSSDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CssDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CssDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stacksimplify/CSSDemo/CssDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.CSSDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CssDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
